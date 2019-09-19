24b38958fabf8c44a0f7304bcf419f15f0b868b7
saddamgit <saddam100895@gmail.com>
2019-09-20 01:14:43 +0700

    first commit

---
+++ application/views/index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="assets/mycss.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/fontawesome-free-5.10.2-web/css/all.css">
+<script src="https://unpkg.com/aos@2.3.0/dist/aos.js"></script>
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+
+<body>
+	<script type="text/javascript">
+	AOS.init({
+		duration: 1200,
+	});
+</script>
+
+	<div class="container-fluid parallax" >
+		<h1 data-aos="fade-up">WISATA JOGJA<p>Nikmati Pesona Kota Gudeg</p></h1>
+	</div>
+		
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6 menu">
+				<ul class="nav nav-pills nav-justified">
+					<li class="nav-item"><a href="#"><div>Restaurant</div></a></li>
+					<li class="nav-item"><a href="#"><div>Hotel</div></a></li>
+					<li class="nav-item"><a href="#"><div>Shopping</div></a></li>
+					<li class="nav-item"><a href="#"><div>Mosque</div></a></li>
+				</ul>
+			</div>
+			<div class="col-md-3"></div>
+		</div>
+	</div>
+
+
+	<section class="text">
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-md-8">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+				<div class="col-md-4" data-aos="zoom-out">
+					<div class="info">Yogyakarta 2019 </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+
+	<section class="destination">
+		<div class="container">
+			<center><h3 data-aos="zoom-out">Our Destinations</h3></center>
+			<div class="row">
+				<div class="col-lg-4" data-aos="fade-right">
+					<a href="#">
+					<img src="assets/candi ijo.jpg" alt="" >
+					<label>Wisata Candi</label>
+					</a>
+				</div>
+				<div class="col-lg-4" data-aos="fade-down">
+					<a href="#">
+					<img src="assets/pok tunggal.jpg" alt="" >
+					<label>Wisata Pantai</label>
+					</a>
+				</div>
+				<div class="col-lg-4" data-aos="fade-left">
+					<a href="#">
+					<img src="assets/jogja bay.jpg" alt="">
+					<label>Wisata Wahana</label>
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4"   data-aos="fade-right" >
+					<a href="#">
+					<img src="assets/kuliner.jpg" alt="">
+					<label>Wisata Kuliner</label>
+					</a>
+				</div>
+				<div class="col-lg-4" data-aos="fade-up">
+					<a href="#">
+					<img src="assets/religi.jpg" alt="" >
+					<label>Wisata Religi</label>
+					</a>
+				</div>
+				<div class="col-lg-4"  data-aos="fade-left">
+					<a href="#">
+					<img src="assets/air terjun.jpg" alt="" >
+					<label>Wisata Alam</label>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="blog">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3" data-aos="fade-right">
+					<center>
+					<label>Blogs</label>
+					</center>
+				</div>
+				<div class="col-md-3 card" data-aos="fade-up">
+					<a href="#">
+					<div class="face face1">
+					<h3>Taman Sari</h3>
+						<div class="content">
+							<img src="assets/taman sari.jpg" alt="" style="width: 100%">
+						</div>
+					</div>
+					</a>
+					<div class="face face2">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+						 </p>
+						<a href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-md-3 card" data-aos="fade-up">
+					<a href="#">
+					<div class="face face1">
+					<h3>Ratu Boko</h3>
+						<div class="content">
+							<img src="assets/ratu boko.jpg" alt="" style="width: 100%">
+						</div>
+					</div>
+					</a>
+					<div class="face face2">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+						 </p>
+						<a href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-md-3 card" data-aos="fade-up">
+					<a href="#">
+					<div class="face face1">
+					<h3>Tebing Breksi</h3>
+						<div class="content">
+							<img src="assets/tebing breksi.jpg" alt="" style="width: 100%">
+						</div>
+					</div>
+					</a>
+					<div class="face face2">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+						 </p>
+						<a href="#">Read More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+
+
+	
+
+	<section class="paket">
+		<div class="container">
+			<center><h3>Paket Jalan-jalan</h3></center>
+			<div class="row">
+				<div class="col-sm-3" data-aos="zoom-in">
+					<div class="list" >
+					<center><label>Paket A</label></center>
+						<ul>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-3" data-aos="zoom-in" >
+					<div class="list" >
+					<center><label>Paket B</label></center>
+						<ul>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-3" data-aos="zoom-in" >
+					<div class="list" >
+					<center><label>Paket C</label></center>
+						<ul>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-3" data-aos="zoom-in" >
+					<div class="list" >
+					<center><label>Paket D</label></center>
+						<ul>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+							<li>Candi Prambanan</li>
+						</ul>
+					</div>
+				</div>
+				<a href="#booknow" id="booknow">Booking Now</a>
+			</div>
+		</div>
+	</section>
+
+
+	<div class="container-fluid footer">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>Office Address</h3>
+				<address>
+					Jln. Kamboja 4 No 126<br>
+					Perumnas Condong Catur
+					Depok Sleman
+					DI. Yogyakarta	
+				</address>
+			</div>
+			
+			<div class="col-sm-4">
+				<h3>Our Relation</h3>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+
+			<div class="col-sm-4 sosmed">
+				<h3>Contact</h3>
+				<i class="fab fa-facebook"></i>
+				<i class="fab fa-instagram"></i>
+				<i class="fab fa-twitter"></i>
+				<i class="fab fa-whatsapp"></i>
+			</div>
+		</div>
+	</div>
+
+	
+</body>
+</html>
